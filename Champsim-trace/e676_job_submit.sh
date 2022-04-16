c5ff6348ddd25acbaba3c6b34740b6bfc60c9cee
Abby P Joby <abbypjoby@Abbys-MacBook-Air-3.local>
2022-04-16 13:20:05 -0500

    change job submit script

--- Champsim-trace/e676_job_submit.sh
+++ Champsim-trace/e676_job_submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=serial_job_test   # Job name
 #SBATCH --mail-type=END,FAIL         # Mail Events (NONE,BEGIN,FAIL,END,ALL)
-#SBATCH --mail-user=NetID@tamu.edu   # Replace with your email address
+#SBATCH --mail-user=abbypjoby@tamu.edu   # Replace with your email address
 #SBATCH --ntasks=1                   # Run on a single CPU
-#SBATCH --time=02:00:00              # Time limit hh:mm:ss
+#SBATCH --time=06:00:00              # Time limit hh:mm:ss
 #SBATCH --output=serial_test_%j.log  # Standard output and error log
 #SBATCH --partition=non-gpu          # This job does not use a GPU
 
